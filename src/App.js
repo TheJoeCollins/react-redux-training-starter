3dcb3531ec41e9b5cb795529f052a2dcf44b814b
Joseph Collins <josephecollins1992@gmail.com>
2018-07-25 17:19:19 -0400

    Added JSON Dummy Data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React with Redux -- Starter</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">Let's get started...</p>
       </div>
     );
   }
